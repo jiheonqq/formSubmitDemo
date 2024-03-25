300bfa3c7fd88b7611ed81e71cb81db835a00389
jiheonqq <163431495+jiheonqq@users.noreply.github.com>
2024-03-25 12:22:42 +0100

    Add files via upload

--- contact_form_handler.php
+++ contact_form_handler.php
@@ -3,18 +3,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $subject = $_POST['subject']
     $message = $_POST['message'];
 
+    $mailheader = "From:" .$name. "<" .$email. ">\r\n";
+
     // Email address where you want to receive the messages
-    $to = "replysmas@gmail.com";
+    $recipient = "replysmas@gmail.com";
 
     // Email subject
     $subject = "New Contact Form Submission";
 
-    // Email content
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n";
-    $email_content .= "Message:\n$message\n";
+    mail($recipient, $subject, $message, $mailheader)
+    or die("Error!")
+    echo"Message sent!";
 
     // Email headers
     $headers = "From: $name <$email>";
